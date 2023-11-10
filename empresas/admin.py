c8a8acf594c3bed28628e6281b97881094457afe
agcpomps <agcpomps@gmail.com>
2023-11-10 17:32:23 +0100

    models almost done

--- empresas/admin.py
+++ empresas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Alvara, Empresa, Especialidade
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ["nome", "telefone", "email", "provincia", "municipio"]
+    list_filter = ["nome", "provincia", "municipio"]
+    search_fields = ["nome", "telefone"]
+    prepopulated_fields = {"slug": ("nome",)}
+    ordering = ["nome"]
--- empresas/models.py
+++ empresas/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Especialidade(models.Model):
+    nome = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['nome'])
+        ]
+        verbose_name = "especialidade"
+        verbose_name_plural = "categorias"
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+
+class Empresa(models.Model):
+    especialidade = models.ForeignKey(Especialidade, related_name="empresas", on_delete=models.CASCADE)
+    nome = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=200)
+    gerente = models.CharField(max_length=250)
+    telefone = models.CharField(max_length=9)
+    email = models.EmailField()
+    morada = models.CharField(max_length=250)
+    provincia = models.CharField(max_length=200, verbose_name="província")
+    municipio = models.CharField(max_length=200, verbose_name="município")
+    nif = models.CharField(max_length=16)
+    class Meta:
+        indexes = [
+            models.Index(fields=['nome'])
+        ]
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Alvara(models.Model):
+    numero = models.CharField(max_length=200)
+    emissao = models.DateField()
+    termino = models.DateField()
+    tipo = models.IntegerField()
+    empresa = models.OneToOneField(Empresa, on_delete=models.CASCADE, related_name="alvara")
+
+class Documento(models.Model):
+    descricao = models.CharField(max_length=255, blank=True)
+    documento = models.FileField(upload_to="documents")
+    
+
+    def __str__(self) -> str:
+        return f"Alvara numero {self.numero} empresa {self.empresa}"
+
+
+    
