51b8768f17c05a0321d7381608f9975eb6bb55e6
agcpomps <agcpomps@gmail.com>
2023-11-13 10:28:38 +0100

    models done

--- empresas/admin.py
+++ empresas/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
 
 
-from .models import Alvara, Empresa, Especialidade
+from .models import Alvara, Empresa, Especialidade, Documento
+
+@admin.register(Especialidade)
+class EspecialidadeEmpresa(admin.ModelAdmin):
+    list_display = ['nome']
+    search_fields = ['nome']
+    prepopulated_fields = {'slug': ('nome',)}
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    list_display = ["nome", "telefone", "email", "provincia", "municipio"]
-    list_filter = ["nome", "provincia", "municipio"]
+    list_display = ["nome", "telefone", "email", "provincia", "municipio", "especialidade"]
+    list_filter = ["nome", "provincia", "municipio", "especialidade"]
     search_fields = ["nome", "telefone"]
     prepopulated_fields = {"slug": ("nome",)}
     ordering = ["nome"]
+
+@admin.register(Alvara)
+class AlvaraAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'emissao', 'termino', 'tipo', 'empresa']
+    list_filter = ['numero', 'termino', 'tipo']
+    search_fields = ['numero']
+    prepopulated_fields = {"slug": ('numero',)}
+    ordering = ['numero']
+
+@admin.register(Documento)
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ['documento', 'descricao']
+    search_fields = ['documento', 'descricao']
+
--- empresas/models.py
+++ empresas/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 
 
@@ -20,13 +21,14 @@ class Empresa(models.Model):
     especialidade = models.ForeignKey(Especialidade, related_name="empresas", on_delete=models.CASCADE)
     nome = models.CharField(max_length=250)
     slug = models.SlugField(max_length=200)
-    gerente = models.CharField(max_length=250)
+    gerente = models.CharField(max_length=250, blank=True)
     telefone = models.CharField(max_length=9)
     email = models.EmailField()
     morada = models.CharField(max_length=250)
     provincia = models.CharField(max_length=200, verbose_name="província")
     municipio = models.CharField(max_length=200, verbose_name="município")
     nif = models.CharField(max_length=16)
+    created_at = models.DateField(auto_now_add=True)
     class Meta:
         indexes = [
             models.Index(fields=['nome'])
@@ -37,6 +39,7 @@ class Empresa(models.Model):
 
 class Alvara(models.Model):
     numero = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     emissao = models.DateField()
     termino = models.DateField()
     tipo = models.IntegerField()
@@ -44,11 +47,20 @@ class Alvara(models.Model):
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=255, blank=True)
-    documento = models.FileField(upload_to="documents")
+    documento = models.FileField(upload_to="documentos/", blank=True, null=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name="documentos")
+    
     
 
     def __str__(self) -> str:
-        return f"Alvara numero {self.numero} empresa {self.empresa}"
-
-
+        return f"Alvara numero {self.documento} empresa {self.empresa}"
     
+    def save(self, *args, **kwargs):
+        if not self.documento:
+            return
+        upload_to = os.path.join('documentos/', self.empresa.nome)
+
+
+        self.documento.field.upload_to = upload_to
+
+        super(Documento, self).save(*args, *kwargs)
