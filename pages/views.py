bf18811894292f2b8f2b38bca091f6ba9739157b
agcpomps <agcpomps@gmail.com>
2024-06-25 11:34:36 +0100

    simple validation form login

--- pages/views.py
+++ pages/views.py
@@ -1,22 +1,30 @@
-from  django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 
 
-from . forms import LoginForm
+from .forms import LoginForm
+
 
 def home_login_page(request):
     form = LoginForm()
-    message = ''
-    if request.method == 'POST':
+    message = ""
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password']
+                request,
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
             )
             if user is not None:
                 login(request, user)
                 return redirect("empresas:empresas")
-            
-        message = 'Login falhou'
-    return render(request, 'home.html', context={'form':form, 'message':message})
+            else:
+                form.add_error(None, "Nome de usuario errado ou a password")
+        else:
+            form.add_error(None, "Nome de  Usuário errado ou a Password")
+
+    else:
+        form = LoginForm()
+
+    return render(request, "home.html", context={"form": form, "message": message})
